739d45f9a36f8c84ee20bc99ca170c72bf648e68
Luiz Henrique Simonato <151886300+LuiZordLord@users.noreply.github.com>
2024-02-20 21:56:25 -0300

    Create Atividade2.java

---
+++ Atividade2.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+a. Nomeproduto
+b. Precoproduto
+c. Marcadoproduto
+d. Quantidadeproduto
+e. _Descricaoproduto
+f. Nomeproduto
+g. _numeroProduto
+h. Precodoproduto
+i. Estoquedisponivel
+j. Totaldeprodutos
