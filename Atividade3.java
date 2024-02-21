34f052010c25bd108b2e3fe1ecbcd8543c4d7e5f
Luiz Henrique Simonato <151886300+LuiZordLord@users.noreply.github.com>
2024-02-20 22:17:16 -0300

    Create Atividade3.java

---
+++ Atividade3.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String Nomealuno
+        int Idadealuno
+        float Nota1aluno
+        float Nota2aluno
+        double Media
+    }
+}
