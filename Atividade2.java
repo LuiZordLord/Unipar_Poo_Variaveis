6d703560ad22d3a14fb78d38e944a352156f5db5
Luiz Henrique Simonato <151886300+LuiZordLord@users.noreply.github.com>
2024-02-20 22:16:40 -0300

    Update Atividade2.java

--- Atividade2.java
+++ Atividade2.java
@@ -3,17 +3,17 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        a. Nomeproduto
+        b. Precoproduto
+        c. Marcadoproduto
+        d. Quantidadeproduto
+        e. _descricaoproduto
+        f. Nomeproduto
+        g. _numeroProduto
+        h. Precodoproduto
+        i. Estoquedisponivel
+        j. Totaldeprodutos
+
     }
 }
 
-a. Nomeproduto
-b. Precoproduto
-c. Marcadoproduto
-d. Quantidadeproduto
-e. _descricaoproduto
-f. Nomeproduto
-g. _numeroProduto
-h. Precodoproduto
-i. Estoquedisponivel
-j. Totaldeprodutos
