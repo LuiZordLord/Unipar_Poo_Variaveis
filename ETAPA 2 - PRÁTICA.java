3658aa423fbeae8be1e829ec8961b54021d5c759
Luiz Henrique Simonato <151886300+LuiZordLord@users.noreply.github.com>
2024-04-15 10:08:54 -0300

    Update ETAPA 2 - PRÁTICA.java

--- ETAPA 2 - PRÁTICA.java
+++ ETAPA 2 - PRÁTICA.java
@@ -456,15 +456,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Criação de objeto (Produto) com o 1º construtor;
         Produto produto1 = new Produto("Produto A");
         System.out.println("Produto 1:");
         System.out.println("Nome: " + produto1.getNome());
         System.out.println("Preço: " + produto1.getPreco());
         System.out.println("Quantidade em estoque: " + produto1.getQuantEstoque());
         System.out.println(" ");
-
-        // Criação de objeto (Produto) com o 2º construtor;
 
         System.out.print("Digite o nome do Produto B: ");
         String nomeProdutoB = scanner.nextLine();
@@ -477,8 +474,6 @@ public class Main {
         System.out.println("Preço: " + produto2.getPreco());
         System.out.println("Quantidade em estoque: " + produto2.getQuantEstoque());
         System.out.println();
-
-        // Criação de obejto (Produto) com o 3º construtor;
 
         System.out.print("Digite o nome do Produto C: ");
         scanner.nextLine();
